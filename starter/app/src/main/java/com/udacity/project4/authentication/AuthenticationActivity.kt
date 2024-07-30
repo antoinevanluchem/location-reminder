a4abf7c484293a4e0d44d1ea35fe5b23f99dfa56
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-07-30 12:16:31 +0200

    Add custom login screen

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -35,14 +36,8 @@ class AuthenticationActivity : AppCompatActivity() {
         binding.loginButton.setOnClickListener {
             launchLogInFlow()
         }
-
-        // TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
     }
 
-    //
-    // LoginFlow
-    //
     private var loginFlowLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -59,10 +54,12 @@ class AuthenticationActivity : AppCompatActivity() {
         val providers = arrayListOf(
             AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
         )
+        val layout = AuthMethodPickerLayout.Builder(R.layout.login_layout)
+            .setEmailButtonId(R.id.email_button).setGoogleButtonId(R.id.google_button).build()
 
-        val intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
-            providers
-        ).build()
+        val intent =
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers)
+                .setAuthMethodPickerLayout(layout).setTheme(R.style.AppTheme).build()
         loginFlowLauncher.launch(intent)
     }
 
