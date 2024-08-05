cf2301ec0a809aa63727bbd233cbb889025ea752
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-05 14:54:57 +0200

    Set up reminders list view model test

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,50 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.koin.core.context.GlobalContext.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    // Subject under test
+    private lateinit var remindersList: RemindersListViewModel
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    // Use a fake data source to be injected into the viewmodel
+    private lateinit var data: FakeDataSource
 
+    private val golden_gate_bridge_reminder = ReminderDTO("Practice diving", "Improve salto", "Golden Gate Bridge", 37.8199, 122.4786,"1")
+    private val arctic_reminder = ReminderDTO("Pet polar bear", "Take snow boots with you", "Arctic", 76.2506, 100.1140, "2")
+    private val mount_etna_reminder = ReminderDTO("Go for a sauna", "Don't forget towel", "Mount Etna", 37.7510, 14.9934, "3")
+
+    //For unit testing, set the primary coroutine dispatcher.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var coroutineRule = MainCoroutineRule()
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun model(){
+        stopKoin()
+        data = FakeDataSource()
+        remindersList = RemindersListViewModel(ApplicationProvider.getApplicationContext(), data)
+    }
+
+    @After
+    fun clearData() = runBlockingTest{
+        data.deleteAllReminders()
+    }
 }
