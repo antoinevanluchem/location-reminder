d7fe4f1dc330e98462052191634b160e316d0c1b
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-05 14:44:34 +0200

    Implement fake data source

--- starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,48 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(private var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    private var shouldReturnError = false
 
-    override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+    fun setReturnError(value:Boolean){
+        shouldReturnError = value
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if(shouldReturnError){
+            return Result.Error("shouldReturnError was set to true in the FakeDataSource")
+        }
+        if (reminders == null) {
+            return Result.Error("reminders == null in the FakeDateSource")
+        }
+
+        return Result.Success(ArrayList(reminders!!))
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if(shouldReturnError){
+            return Result.Error("shouldReturnError was set to true in the FakeDataSource")
+        }
+        if (reminders == null) {
+            return Result.Error("reminders == null in the FakeDateSource")
+        }
+
+
+        val foundReminder = reminders?.find { it.id == id }
+
+        if (foundReminder == null) {
+            return Result.Error("Reminder with id $id could not be found in the FakeDataSoruce")
+        }
+
+        return Result.Success(foundReminder)
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
-
-
 }
