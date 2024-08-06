58806b763e806f04b2508d363eb425b9de41dfaa
Antoine Van Luchem <antoine.van-luchem@bmw.de>
2024-08-06 13:02:55 +0200

    Add tests in reminder list fragment test

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -1,13 +1,24 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.R
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.local.FakeDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.runner.RunWith
@@ -18,14 +29,19 @@ import org.koin.test.KoinTest
 import org.koin.test.inject
 import org.koin.dsl.module
 import org.koin.androidx.viewmodel.dsl.viewModel
-
+import org.mockito.Mockito.mock
+import kotlin.test.Test
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.CoreMatchers.not
+import org.mockito.Mockito.verify
 
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 //UI Testing
 @MediumTest
-class ReminderListFragmentTest: KoinTest {
+class ReminderListFragmentTest : KoinTest {
 
     private val dataSource: ReminderDataSource by inject()
 
@@ -43,12 +59,11 @@ class ReminderListFragmentTest: KoinTest {
         val myModule = module {
             viewModel {
                 RemindersListViewModel(
-                    get(),
-                    get()
+                    get(), get()
                 )
             }
             single {
-                FakeDataSource()
+                FakeDataSource() as ReminderDataSource
             }
         }
         startKoin {
@@ -60,4 +75,46 @@ class ReminderListFragmentTest: KoinTest {
     @After
     fun deleteAllReminders() = runBlocking { dataSource.deleteAllReminders() }
 
+    @Test
+    fun savedRemindersAreDisplayed() = runTest {
+        dataSource.saveReminder(goldenGateBridgeReminder)
+        dataSource.saveReminder(arcticReminder)
+        dataSource.saveReminder(mountEtnaReminder)
+
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+
+        onView(withText(goldenGateBridgeReminder.title)).check(matches(isDisplayed()))
+        onView(withText(arcticReminder.description)).check(matches(isDisplayed()))
+        onView(withText(mountEtnaReminder.title)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.noDataTextView)).check(matches(not(isDisplayed())))
+    }
+
+    @Test
+    fun noDataTextViewDisplayed() = runTest {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withText(R.string.no_data)).check(matches(isDisplayed()))
+
+        onView(withText(goldenGateBridgeReminder.title)).check(doesNotExist())
+        onView(withText(arcticReminder.description)).check(doesNotExist())
+        onView(withText(mountEtnaReminder.title)).check(doesNotExist())
+    }
+
+
+    @Test
+    fun clickButtonAndNavigate() = runTest {
+        val scenario = launchFragmentInContainer<ReminderListFragment>(Bundle(), R.style.AppTheme)
+        val navController = mock(NavController::class.java)
+        scenario.onFragment { Navigation.setViewNavController(it.view!!, navController) }
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        verify(navController).navigate(ReminderListFragmentDirections.toSaveReminder())
+    }
 }
